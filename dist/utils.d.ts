4ca9e043a03b3fb2f8eca0e21aef3a25255c1b14
atomoxetine <emilybertollo@outlook.com>
2024-02-07 01:44:30 -0300

    removed dist

--- dist/utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function objToRevMap(obj: Object): Map<string, string>;
-export declare function objToValArray(obj: Object): Array<string>;
-export declare function objToValInitials(obj: Object): Array<String>;
